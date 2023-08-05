2207aa66406331b800e2f066153c010e6678fb54
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-08-05 18:09:51 +0600

    get element by index,set element by index,find the element index number

--- array.js
+++ array.js
@@ -5,3 +5,9 @@ console.log(friends);
 console.log(numbers.length); //array length
 console.log(friends.length); //array length
 console.log(friends[1]); // array index
+var element = numbers[3]; // get element by index
+console.log(element);
+numbers[0] = 400; // set element by index
+console.log(numbers);
+var indexPosition = numbers.indexOf(1000); //find the element index number
+console.log(indexPosition);

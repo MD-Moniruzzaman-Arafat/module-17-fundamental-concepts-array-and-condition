27f2052e12cc5f09a97f2310d14ccd2185d62b1e
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-08-05 17:51:34 +0600

    declare array, array length, array index

---
+++ array.js
@@ -0,0 +1,7 @@
+var numbers = [10,20,30,40,50,60,70,80,90,100];
+var friends = ['salman','atik','mohit','hasan'];
+console.log(numbers);
+console.log(friends);
+console.log(numbers.length); //array length
+console.log(friends.length); //array length
+console.log(friends[1]); // array index
